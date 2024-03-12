d7c3f5847fbd02c7371607fde29d718e1fb32ece
shreyJS <deshpandeshreyas850@gmail.com>
2024-03-12 19:37:17 +0530

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      profilePicture: {
+        type: String,
+        default:
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      },
+      // not accessible from UI, done manually
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
+      fullName: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+      },
+    },
+    { timestamps: true }
+  );
+  
+  const User = mongoose.model("User", userSchema);
+  export default User;
