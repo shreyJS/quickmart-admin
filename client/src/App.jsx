7349aac54194fc555919f7b1cd32bd24faf975be
shreyJS <deshpandeshreyas850@gmail.com>
2024-03-12 16:55:15 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
 export default function App() {
-  return <div className="text-3xl text-red-500">hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<SignIn/>}/>
+        <Route path="/sign-up" element ={<SignUp/>}/>
+        <Route path="/dashboard" element ={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
